08ddd56a714435e64df7e8fd00581c95fc3df9f3
Parthgupta0605 <cs1230507@iitd.ac.in>
2025-03-20 11:26:09 +0530

    implemented processor.hpp

---
+++ src/processor.hpp
@@ -0,0 +1,71 @@
+#ifndef PROCESSOR_HPP
+#define PROCESSOR_HPP
+
+#include "instruction.hpp"
+#include <vector>
+#include <cstdint>
+
+class Processor {
+public:
+    Processor();
+    void loadProgram(const std::vector<uint32_t>& program);
+    void run();
+
+private:
+    uint32_t registers[32];
+    uint32_t pc;
+    std::vector<uint32_t> memory;
+
+    struct IF_ID {
+        uint32_t pc;
+        Instruction instr;
+    } if_id;
+
+    struct ID_EX {
+        uint32_t pc;
+        std::string opcode;
+        int rd;
+        int64_t imm;
+        int funct3, funct7;
+        int regVal1, regVal2;
+
+        bool ALUSrc;
+        uint8_t ALUOp;
+        bool Branch;
+        bool MemRead;
+        bool MemWrite;
+        bool RegWrite;
+        bool MemToReg;
+    } id_ex;
+
+    struct EX_MEM {
+        uint32_t pc_imm;
+        uint32_t aluResult;
+        uint32_t regVal2;
+        int rd;
+        bool zero;
+
+        bool Branch;
+        bool MemRead;
+        bool MemWrite;
+        bool RegWrite;
+        bool MemToReg;
+    } ex_mem;
+
+    struct MEM_WB {
+        uint32_t memData;
+        uint32_t aluResult;
+        int rd;
+
+        bool RegWrite;
+        bool MemToReg;
+    } mem_wb;
+
+    void fetch();
+    void decode();
+    void execute();
+    void memoryAccess();
+    void writeBack();
+};
+
+#endif
